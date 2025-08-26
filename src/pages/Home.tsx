b00a86e3f0e78268b7440613c48434bfd999ea1a
Sam Whorton <samwhorton@Hannahs-MacBook-Air.local>
2025-08-26 15:54:20 -0400

    Initial Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is my React + TypeScript + Vite portfolio site.</p>
+    </div>
+  );
+};
+
+export default Home;
