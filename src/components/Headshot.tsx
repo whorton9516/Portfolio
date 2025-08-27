a1e54e4fac6e3289fa15adcf1b1abc7d2dbddea7
Samm Whorton <samwhorton@Laurens-MacBook-Pro-2.local>
2025-08-26 20:10:26 -0400

    various updates

--- src/components/Headshot.tsx
+++ src/components/Headshot.tsx
@@ -1,11 +1,12 @@
 import pic from '../assets/me.jpg';
 
 const Headshot = () => {
-    return (
-        <img src={pic}
-             style={{ height: '300px', borderRadius: '50%' }}
-        />
-    );
+   return (
+      <img
+         src={pic}
+         style={{ height: '300px', borderRadius: '50%' }}
+      />
+   );
 }
 
 export default Headshot;
